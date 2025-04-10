18db191b4d8e5a25eb99e1ab3e74ea65a462a0d5
Noelia Santana Roda <noelia.sroda@gmail.com>
2025-04-10 01:52:55 -0300

    segundo commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,11 +6,19 @@ exports.login = (req, res) => {
         email: "admin@empresa.com",
         password: "1234",
       };
-
+    
     if (username === adminUser.email && password === adminUser.password) {
       return res.status(200).json({ message: "Login exitoso", token: "ADMIN-TOKEN" });
   } else {
     return res.status(401).json({ message: "Credenciales incorrectas" });
   }
 };
-  
+exports.validateToken = (req, res) => {
+  const { token } = req.body;
+
+  if (token === "ADMIN-TOKEN") {
+    return res.status(200).json({ valid: true });
+  } else {
+    return res.status(401).json({ valid: false });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
+router.post('/validate', authController.validateToken);
 
 module.exports = router;
